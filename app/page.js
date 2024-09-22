5141030ef511c7a2c568ac77f35a3f8d71e52c3e
saumay3105 <112512912+saumay3105@users.noreply.github.com>
2024-09-22 20:21:04 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,19 @@
-import React from "react";
-
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-cente flex-col">
+      <h1 className="head_text text center">
+        Discover & Share
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient">AI Powered Prompts</span>
+      </h1>
+      <p className="desc text-center"> 
+        Promptog is an open-source AAI prompting tool for 
+        modern world to discover, create and share creative
+        prompts for GenAI.
+      </p>
+      {/* Feed */}
+    </section>
+  );
 };
 
 export default Home;
