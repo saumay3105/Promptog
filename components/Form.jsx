73f9991ae1ed93343c58082e5d134c38e1fa870a
saumay3105 <112512912+saumay3105@users.noreply.github.com>
2024-10-01 20:46:21 +0530

    Update Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,16 +1,25 @@
 import Link from "next/link";
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
-  return( <section className="w-full max-w-full flex-start flex-col">
-    <h1 className="head_text text-left">
-      <span className="blue_gradient">{type} Post</span>
-    </h1>
-    <p className="desc text-left max-w-md">
-      {type} and share amazing propmpts with the world, and let your imagination
-      run wild with any AI-powered platform{" "}
-    </p>
-    <form></form>
-  </section>);
+  return (
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing propmpts with the world, and let your
+        imagination run wild with any AI-powered platform{" "}
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-1-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label> 
+          <span className="font-satoshi ">Your AI prompt</span>
+        </label>
+      </form>
+    </section>
+  );
 };
 
 export default Form;
