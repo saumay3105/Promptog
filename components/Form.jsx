73405c77158eefb45023fdde5346905a83595d3d
saumay3105 <112512912+saumay3105@users.noreply.github.com>
2024-10-02 22:07:26 +0530

    Update Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -14,9 +14,40 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
         onSubmit={handleSubmit}
         className="mt-10 w-full max-1-2xl flex flex-col gap-7 glassmorphism"
       >
-        <label> 
-          <span className="font-satoshi ">Your AI prompt</span>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI prompt
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) => ({ ...post, prompt: e.target.value })}
+            placeholder="Write your prompt here"
+            required
+            className="form_textarea"
+          />
         </label>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag <span className="font-normal">(#product,#webdev, #idea)</span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => ({ ...post, tag: e.target.value })}
+            placeholder="#tag"
+            required
+            className="form_input"
+          />
+        </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/" className="text-gray-500 text-sm">
+            {" "}
+            Cancel
+          </Link>
+          <button type="submit" disabled={submitting} className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white ">
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
       </form>
     </section>
   );
