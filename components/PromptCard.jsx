4e6b1fcd2e92236471070eb2d0db02a383b560be
saumay3105 <112512912+saumay3105@users.noreply.github.com>
2024-10-03 23:52:12 +0530

    Update PromptCard.jsx

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,13 +7,17 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
   return (
     <div className="propmpt_card">
       <div className="flex justify-between items-start gap-5">
-        <div>
+        <div className="flex-1 flex justify-center gap-3 cursor-pointer">
           <Image
             src={post.creator.image}
             alt="user_image"
             width={40}
             height={40}
           />
+          <div className="flex flex-col">
+            <h3>{post.creator.username}</h3>
+            <p>{post.creator.email}</p>
+          </div>
         </div>
       </div>
     </div>
