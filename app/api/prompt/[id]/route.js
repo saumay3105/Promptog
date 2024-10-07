9fd3a91b9a82c90f7cb22fe6caeaea3f8880161a
saumay3105 <112512912+saumay3105@users.noreply.github.com>
2024-10-07 23:09:22 +0530

    Update route.js

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,1 +1,36 @@
-//GET 
+//GET (read)
+
+//DELETE (delete)
+
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request, { params }) => {
+  try {
+    await connectToDB();
+
+    const prompt = await Prompt.findById(params.id).populate("creator");
+    if (!prompt) return new Response("Prompt not found", { status: 404 });
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Failed to fetch all prompts", {
+      status: 500,
+    });
+  }
+};
+
+//PATCH(update)
+
+export const PATCH = async (request, { params }) => {
+  const { prompt, tag } = await request.json();
+
+  try {
+    await connectToDB();
+
+    const existingPrompt = await Prompt.findById(params.id);
+    if (!existingPrompt)
+      return new Response("Prompt not found", { status: 404 });
+  } catch (error) {}
+};
