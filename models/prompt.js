ed77ebb56506f6a98a84aefba6d3b7edc187bc18
saumay3105 <112512912+saumay3105@users.noreply.github.com>
2025-02-26 21:01:18 +0530

    Update prompt.js

--- models/prompt.js
+++ models/prompt.js
@@ -1,21 +1,28 @@
-import  { Schema, model, models } from "mongoose";
-
+import { Schema, model, models } from "mongoose";
 
 const PromptSchema = new Schema({
-    creator : {
-        type :  Schema.Types.ObjectId,
-        ref: 'User',
-    },
-    prompt: {
-        type: String,
-        required : [true, 'Prompt is necessary']
-    },
-    tag:{
-        type: String,
-        required : [true, 'tag is necessary']
-    }
-})
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: String,
+    required: [true, "Prompt is necessary"],
+  },
+  tag: {
+    type: String,
+    required: [true, "tag is necessary"],
+  },
+  upvotes: {
+    type: Number,
+    default: 0,
+  },
+  downvotes: {
+    type: Number,
+    default: 0,
+  },
+});
 
-const Prompt = models.Prompt || model('Prompt', PromptSchema);
+const Prompt = models.Prompt || model("Prompt", PromptSchema);
 
 export default Prompt;
